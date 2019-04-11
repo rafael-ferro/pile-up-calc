d7235b00dbff4c3ce9256f6757d041db409bc8dc
Rafael Ferro <rafael.ferro@usp.br>
2019-04-10 21:08:13 -0300

    Split pile up spectrum into lost and gained counts

--- barradas_reis_xrs35_2006.f90
+++ barradas_reis_xrs35_2006.f90
@@ -1,5 +1,5 @@
 !-----------------------------------------------------------------------
-subroutine pileup(n, spectrum, f, Tw, Tp, time, pile) 
+subroutine pileup(n, spectrum, f, Tw, Tp, time, L, G)
 ! Calculate pile-up according to Barradas & Reis XRS 35 (2006).
 !  Tw     shapping time (s)
 !  Tp     time required for the pulse to reach the maximum value
@@ -8,18 +8,20 @@ subroutine pileup(n, spectrum, f, Tw, Tp, time, pile)
 !  Tpulse = f*Tw, f depends on the amplifier (ref f=2)
 !-----------------------------------------------------------------------
     implicit none
-    
+
     integer, parameter :: dp = kind(1.d0)
     integer, intent(in) :: n
     real(dp), intent(in) :: f, Tw, Tp, time
     real(dp), dimension(n), intent(in) :: spectrum
-    real(dp), dimension(n), intent(out) :: pile
+    real(dp), dimension(2*n), intent(out) :: L, G
     real(dp) :: Nt, Tpulse, ixj, LG
     integer :: i, j, k, ipj
 
     Tpulse = f * Tw
     Nt = sum(spectrum) / time
-    pile = 0
+    L = 0
+    G = 0
+
     do i = 1, n
         if (spectrum(i) .le. 0) cycle
         do j = i, n
@@ -28,14 +30,13 @@ subroutine pileup(n, spectrum, f, Tw, Tp, time, pile)
             ixj = i*j
             do k = j, ipj
                 LG = spectrum(i) * spectrum(j) * (sqrt((ipj-k+1.0)*ipj/ixj) - sqrt((ipj-k)*ipj/ixj))
-                pile(i) = pile(i) - LG
-                pile(j) = pile(j) - LG
-                if (k <= n) then
-                    pile(k) = pile(k) + LG
-                end if
+                L(i) = L(i) - LG
+                L(j) = L(j) - LG
+                G(k) = G(k) + LG
             end do
         end do
     end do
-   pile = pile * exp(-Nt * Tpulse) * Tp / time
+   L = L * exp(-Nt * Tpulse) * Tp / time
+   G = G * exp(-Nt * Tpulse) * Tp / time
 
 end subroutine pileup
